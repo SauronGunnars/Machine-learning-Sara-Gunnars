f4dfde0e038a7cee5cd9df82e02aba8d43349f22
Sara <gunnars.sara@gmail.com>
2023-06-16 07:24:15 +0200

    Dividing up x and y

--- Lab/production_model.py
+++ Lab/production_model.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.metrics import classification_report
 import joblib
 
+#loading sample
 test_sample = pd.read_csv("Data/test_sample.csv")
 
-print(test_sample.head())
+#Dividing  up x and values
+X,y = test_sample.drop('cardio', axis  = 1), test_sample['cardio']
